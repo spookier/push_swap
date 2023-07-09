90fa4f6b52dfad9e935740d427dd5fbd4d7d099e
spookier <costinalexandru777@gmail.com>
2023-07-10 00:45:09 +0200

    more error fix

--- srcs/checker/main.c
+++ srcs/checker/main.c
@@ -32,12 +32,34 @@ void print_stack(t_stack **a)
 
 int error_free_and_exit(t_stack **stack_a, char *operation)
 {
-	write(1, "KO\n", 3);
+	write(1, "Error\n", 6);
 	if(stack_a)
 		free_stack(stack_a);
 	if(operation)
 		free(operation);
 	exit(-1);
+}
+
+void check_program(t_stack **stack_a)
+{
+	if (check_duplicate(*stack_a) == 1)
+	{
+		write(1, "Error\n", 6);
+		if(stack_a)
+			free_stack(stack_a);
+		exit(-1);
+	}
+	if(start_operations(stack_a) == -1)
+	{
+		write(1, "KO\n", 3);
+		if(stack_a)
+			free_stack(stack_a);
+		exit(-1);
+	}
+	if(check_if_sorted(*stack_a) == 1)
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
 }
 
 int main(int argc, char **argv)
@@ -47,37 +69,15 @@ int main(int argc, char **argv)
 
 	i = 1;
 	stack_a = NULL;
+	if(argc <= 1)
+		return(-1);
 	while (i < argc)
 	{
 		process_arg_and_add_to_stack(&stack_a, argv[i]);
 		i++;
 	}
-	
-	// // @ DEBUG --------------------------
-	// printf("----------------------\n");
-	// print_stack(&stack_a);
-	// printf("----------------------\n");
-	// // ----------------------------------
-
-	if(start_operations(&stack_a) == -1)
-	{
-		write(1, "KO\n", 3);
-		if(stack_a)
-			free_stack(&stack_a);
-		return(-1);
-	}
-
-	// // @ DEBUG --------------------------
-	// printf("----------------------\n");
-	// print_stack(&stack_a);
-	// printf("----------------------\n");
-	// // ----------------------------------
-
-	if(check_if_sorted(stack_a) == 1)
-		write(1, "OK\n", 3);
-	else
-		write(1, "KO\n", 3);
-
-	free_stack(&stack_a);
+	check_program(&stack_a);
+	if(stack_a)
+		free_stack(&stack_a);
 	return(0);
 }
